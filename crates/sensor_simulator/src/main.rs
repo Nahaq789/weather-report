43232bce0f8a6c6fb2cedc12a953df10b24ba117
Nahaq789 <naha022677@icloud.com>
2025-04-04 08:05:22 +0900

    sample sensor gen

--- crates/sensor_simulator/src/main.rs
+++ crates/sensor_simulator/src/main.rs
@@ -1,3 +1,13 @@
+use sensor_simulator::sensor::{
+    location::area::Area,
+    season::{self, Season},
+    Sensor,
+};
+
 fn main() {
+    let area = Area::Tokyo;
+    let season = Season::Spring;
+    let sensor = Sensor::new(&area, &season);
+    println!("{:?}", sensor);
     println!("Hello, world!");
 }

8bf2164e81bb512f6dbff5defa48044a4ff871e1
Nahaq789 <naha022677@icloud.com>
2025-04-05 12:46:01 +0900

    send message

--- crates/sensor_simulator/src/main.rs
+++ crates/sensor_simulator/src/main.rs
@@ -6,9 +6,26 @@ use sensor_simulator::{
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let producer = build_producer();
-    let area = Area::Tokyo;
-    let season = Season::Spring;
-    let sensor = Sensor::new(&area, &season);
-    let _ = send_message(&producer, sensor).await;
+    let mut tasks = Vec::new();
+    for i in 0..1_000_000 {
+        let producer_clone = producer.clone();
+        let task = tokio::spawn(async move {
+            let area = Area::Tokyo;
+            let season = Season::Spring;
+            let sensor = Sensor::new(&area, &season);
+
+            if i % 1000 == 0 {
+                println!("Sending message {}", i);
+            }
+            let _ = send_message(&producer_clone, sensor).await;
+        });
+        tasks.push(task);
+    }
+
+    for task in tasks {
+        let _ = task.await?;
+    }
+
+    println!("All messages sent");
     Ok(())
 }

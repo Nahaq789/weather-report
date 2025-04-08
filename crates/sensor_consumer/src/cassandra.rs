e8a37a3994b9cf6bca4654b5ec67c339bea5aaf8
Nahaq789 <naha022677@icloud.com>
2025-04-09 07:32:14 +0900

    feat: cassandra statement

--- crates/sensor_consumer/src/cassandra.rs
+++ crates/sensor_consumer/src/cassandra.rs
@@ -1,4 +1,4 @@
-use cassandra_cpp::{Cluster, Session};
+use cassandra_cpp::{BindRustType, Cluster, Session, Statement};
 
 pub fn connect_cluster() -> std::pin::Pin<
     Box<dyn std::future::Future<Output = Result<Session, cassandra_cpp::Error>> + Send>,
@@ -14,10 +14,32 @@ pub fn connect_cluster() -> std::pin::Pin<
     Box::pin(async move { cluster.connect_async().await })
 }
 
-// TODO
 pub fn save_sensor<'a>(
     session: &'a Session,
-    data: String,
-) -> std::pin::Pin<Box<dyn std::future::Future<Output = anyhow::Result<()>> + Send + '_>> {
-    Box::pin(async move { Ok(()) })
+    data: &'a str,
+) -> std::pin::Pin<Box<dyn std::future::Future<Output = anyhow::Result<()>> + Send + 'a>> {
+    Box::pin(async move {
+        let query = "INSERT INTO sensor_key_space.sensor (sensor_id, time_stamp, area, latitude, longitude, temperature, humidity, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+        let mut statement = Statement::new(query, 8);
+        map_err(statement.bind(0, data), "Failed to bind sensor_id")?;
+        map_err(statement.bind(1, data), "Failed to bind time_stamp")?;
+        map_err(statement.bind(2, data), "Failed to bind area")?;
+        map_err(statement.bind(3, data), "Failed to bind latitude")?;
+        map_err(statement.bind(4, data), "Failed to bind longitude")?;
+        map_err(statement.bind(5, data), "Failed to bind temperature")?;
+        map_err(statement.bind(6, data), "Failed to bind humidity")?;
+        map_err(statement.bind(7, data), "Failed to bind status")?;
+        map_err(session.execute(&statement).await, "Failed to execute quest")?;
+        Ok(())
+    })
+}
+
+fn map_err<T, E>(result: Result<T, E>, msg: &str) -> anyhow::Result<T>
+where
+    E: std::fmt::Display,
+{
+    match result {
+        Ok(val) => Ok(val),
+        Err(e) => Err(anyhow::anyhow!("{}: {}", msg, e)),
+    }
 }

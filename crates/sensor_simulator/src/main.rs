3c317ea7746df788536888c5ad4366e87c7a0629
Nahaq789 <naha022677@icloud.com>
2025-04-23 07:00:58 +0900

    simulator loop

--- crates/sensor_simulator/src/main.rs
+++ crates/sensor_simulator/src/main.rs
@@ -1,29 +1,39 @@
+use std::time::Duration;
+
+use rand::Rng;
 use sensor::sensor::{location::area, season, Sensor};
 use sensor_simulator::kafka::producer::{build_producer, send_message};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let producer = build_producer()?;
-    let mut tasks = Vec::new();
-    for i in 0..100000 {
-        let producer_clone = producer.clone();
-        let task = tokio::spawn(async move {
-            let area = area::Area::Tokyo;
-            let season = season::Season::Spring;
-            let sensor = Sensor::new(&area, &season);
+    let mut rng = rand::thread_rng();
 
-            if i % 1000 == 0 {
-                println!("Sending message {}", i);
-            }
-            let _ = send_message(&producer_clone, sensor).await;
-        });
-        tasks.push(task);
+    loop {
+        let mut tasks = Vec::new();
+        for i in 0..100000 {
+            let producer_clone = producer.clone();
+
+            let area_value = rng.gen_range(0..99);
+            let season_value = rng.gen_range(1..12);
+            let task = tokio::spawn(async move {
+                let area = area::Area::build(area_value);
+                let season = season::Season::build(season_value);
+                let sensor = Sensor::new(&area, &season);
+
+                if i % 1000 == 0 {
+                    println!("Sending message {}", i);
+                }
+                let _ = send_message(&producer_clone, sensor).await;
+            });
+            tasks.push(task);
+        }
+
+        println!("All messages sent");
+        tokio::time::sleep(Duration::from_secs(3)).await;
+
+        for task in tasks {
+            let _ = task.await?;
+        }
     }
-
-    for task in tasks {
-        let _ = task.await?;
-    }
-
-    println!("All messages sent");
-    Ok(())
 }

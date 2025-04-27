4cbe6ee8cea5c9abc228c6d10bb8dbcb8c4f63f1
Nahaq789 <naha022677@icloud.com>
2025-04-28 08:22:00 +0900

    add gsi

--- scripts/init.dynamodb.sh
+++ scripts/init.dynamodb.sh
@@ -2,7 +2,23 @@ aws dynamodb \
   --endpoint-url http://localhost:8000 \
   create-table \
   --table-name Sensor \
-  --attribute-definitions AttributeName=sensor_id,AttributeType=S \
-  --key-schema AttributeName=sensor_id,KeyType=HASH \
-  --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=5 \
+  --attribute-definitions \
+    AttributeName=sensor_id,AttributeType=S \
+    AttributeName=time_stamp,AttributeType=S \
+    AttributeName=area,AttributeType=S \
+  --key-schema \
+    AttributeName=sensor_id,KeyType=HASH \
+    AttributeName=time_stamp,KeyType=RANGE \
+  --global-secondary-indexes \
+    "[
+      {
+        \"IndexName\": \"AreaIndex\",
+        \"KeySchema\": [
+          {\"AttributeName\": \"area\", \"KeyType\": \"HASH\"}
+        ],
+        \"Projection\": {
+          \"ProjectionType\": \"ALL\"
+        }
+      }
+    ]" \
   --billing-mode=PAY_PER_REQUEST

45e8b35db9e6bdd6e5d2da400a26784985fc93c8
Nahaq789 <naha022677@icloud.com>
2025-04-29 08:59:35 +0900

    fix: dynamo

--- scripts/init.dynamodb.sh
+++ scripts/init.dynamodb.sh
@@ -3,22 +3,9 @@ aws dynamodb \
   create-table \
   --table-name Sensor \
   --attribute-definitions \
-    AttributeName=sensor_id,AttributeType=S \
-    AttributeName=time_stamp,AttributeType=S \
     AttributeName=area,AttributeType=S \
+    AttributeName=time_stamp,AttributeType=S \
   --key-schema \
-    AttributeName=sensor_id,KeyType=HASH \
+    AttributeName=area,KeyType=HASH \
     AttributeName=time_stamp,KeyType=RANGE \
-  --global-secondary-indexes \
-    "[
-      {
-        \"IndexName\": \"AreaIndex\",
-        \"KeySchema\": [
-          {\"AttributeName\": \"area\", \"KeyType\": \"HASH\"}
-        ],
-        \"Projection\": {
-          \"ProjectionType\": \"ALL\"
-        }
-      }
-    ]" \
   --billing-mode=PAY_PER_REQUEST

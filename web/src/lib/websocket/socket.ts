44151827a2cb85c008ee85627fe998bb448861ef
Nahaq789 <naha022677@icloud.com>
2025-05-19 16:07:45 +0900

    fix

--- web/src/lib/websocket/socket.ts
+++ web/src/lib/websocket/socket.ts
@@ -1,36 +1,7 @@
 import { atom, useAtom } from "jotai";
 import { useEffect, useState } from "react";
 
-const url = "ws://localhost:5678/hoge";
-
 const socketAtom = atom<WebSocket | null>(null);
-
-export const useSensor = () => {
-	const [socket, setSocket] = useAtom(socketAtom);
-
-	useEffect(() => {
-		setSocket((socket) => {
-			if (socket) return socket;
-			return new WebSocket(url);
-		});
-	}, [setSocket]);
-
-	useEffect(() => {
-		if (socket) {
-			socket.onmessage = (res) => {
-				console.log(res);
-			};
-		}
-	}, [socket]);
-
-	const sendMessage = (some: string) => {
-		socket?.send("hoge");
-	};
-
-	return {
-		sendMessage,
-	};
-};
 
 export const useWebSocket = (url: string) => {
 	const [socket, setSocket] = useAtom(socketAtom);

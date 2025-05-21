658f9b391fb135ee6d6e0e02e670782ec9d8fd78
Nahaq789 <naha022677@icloud.com>
2025-05-21 19:57:50 +0900

    fix: timeChart

--- web/src/components/charts/timeChart.tsx
+++ web/src/components/charts/timeChart.tsx
@@ -1,52 +1,55 @@
 import { Sensor } from "@/models/sensor/sensor";
+import { ApexOptions } from "apexcharts";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
 
 const Chart = dynamic(() => import("react-apexcharts"), {
-  ssr: false,
-  loading: () => <div>loading...</div>
-})
+	ssr: false,
+	loading: () => <div>loading...</div>,
+});
 
 interface TimeChartProps {
-  sensors: Sensor[]
+	sensors: Sensor[];
 }
 
+const timeLine = ["現在", "5秒", "10秒", "15秒", "20秒", "25秒", "30秒", "35秒", "40秒", "45秒", "50秒", "55秒", "60秒"].reverse();
 const TimeChart = ({ sensors }: TimeChartProps) => {
-  const series = [{
-    name: "temperature",
-    data: [23, 24, 25, 13, 47, 23, 89, 21]
-  },
-  {
-    name: "humidity",
-    data: [11, 45, 26, 130, 7, 3, 14, 32]
-  },
-  {
-    name: "hoge",
-    data: sensors.map(sensor => sensor.aggregate.temperature.avg)
-  }
-  ];
-  const options = {
-    chart: {
-      height: "100%"
-    }
-  };
-  const [isClient, setIsClient] = useState(false);
+	const categories = timeLine.slice(timeLine.length - sensors.length, 14);
+	console.log(timeLine.slice(timeLine.length - sensors.length, 14));
+	const series = [
+		{
+			name: "hoge",
+			data: sensors.map((sensor) => sensor.aggregate.temperature.avg),
+		},
+	];
+	const options: ApexOptions = {
+		chart: {
+			height: "100%",
+		},
+		stroke: {
+			curve: "smooth"
+		},
+		xaxis: {
+			categories: categories,
+		}
+	};
+	const [isClient, setIsClient] = useState(false);
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
 
-  if (!isClient) {
-    return <div>loading...</div>
-  }
+	if (!isClient) {
+		return <div>loading...</div>;
+	}
 
-  return (
-    <>
-      <div className="w-full h-full">
-        <Chart type="line" options={options} series={series}></Chart>
-      </div >
-    </>
-  )
-}
+	return (
+		<>
+			<div className="w-full h-full">
+				<Chart type="line" options={options} series={series}></Chart>
+			</div>
+		</>
+	);
+};
 
 export default TimeChart;

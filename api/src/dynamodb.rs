a03bfa87e4aef5e87401f8f5329a291d79c0ebc6
Nahaq789 <naha022677@icloud.com>
2025-05-27 08:29:50 +0900

    docker api

--- api/src/dynamodb.rs
+++ api/src/dynamodb.rs
@@ -1,9 +1,13 @@
+use std::env;
+
 use aws_sdk_dynamodb::Client;
 
 pub async fn build_client() -> anyhow::Result<Client> {
+    let endpoint_url =
+        env::var("DYNAMO_ENDPOINT").unwrap_or_else(|_| "http://localhost:8000".to_string());
     let config = aws_config::defaults(aws_config::BehaviorVersion::latest())
         .test_credentials()
-        .endpoint_url("http://localhost:8000")
+        .endpoint_url(&endpoint_url)
         .load()
         .await;
     let dynamodb_local = aws_sdk_dynamodb::config::Builder::from(&config).build();
